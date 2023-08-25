7ee3e2982c55e85bc3d566a7808985d233a901b4
mbotsu <m.otsu@infocom.co.jp>
2023-08-25 19:21:36 +0900

    Initial Commit

---
+++ WebViewExample/WebViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewExampleApp.swift
+//  WebViewExample
+//
+//  Created by lamb on 2023/08/25.
+//
+
+import SwiftUI
+
+@main
+struct WebViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
